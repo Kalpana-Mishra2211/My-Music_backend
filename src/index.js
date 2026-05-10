bf70442bc5ce330b554239ef35ca18172a5f2159
Kalpana-Mishra2211 <Kalpana@dojapp.com>
2026-05-10 22:23:15 +0530

    changes done in index

--- src/index.js
+++ src/index.js
@@ -19,15 +19,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true, 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-app.use(cors({
-    origin: '*',
-    credentials: true,
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 //user
@@ -36,8 +30,6 @@ app.use("/api",musicRoutes)
 app.use("/api",favAndFollowRoutes)
 app.use("/api",artistRoutes)
 app.use("/api",playlistRoutes)
-
-
 
 //admin
 app.use("/api/admin/auth",adminAuthRoutes)
